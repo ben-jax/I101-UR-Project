fdc0fe4bb5f514cbd84cc70998f70ce3c342fca2
BenJacobs24 <benjaminjacobs341@gmail.com>
2024-10-23 20:52:12 -0400

    added page files, made side bar

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+simple js functions for side nav bar animations
+this is probably all the js we will need in this porject
+*/
+
+function openNav() {
+    document.getElementById("my-side-nav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("my-side-nav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+  }
