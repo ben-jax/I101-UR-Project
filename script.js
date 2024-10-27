1f25660f329bc865e53e682c603f5f7a5c1e29a6
BenJacobs24 <benjaminjacobs341@gmail.com>
2024-10-27 18:12:05 -0400

    changes, added pages

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ this is probably all the js we will need in this porject
 function openNav() {
     document.getElementById("my-side-nav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("bar").style.marginLeft = "250px";
   }
   
   function closeNav() {
     document.getElementById("my-side-nav").style.width = "0px";
-    document.getElementById("main").style.marginLeft = "0px";
+    document.getElementById("main").style.marginLeft = "auto";
+    document.getElementById("bar").style.marginLeft = "0px";
   }
