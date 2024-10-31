39286bdb8431940af4ef6a908cf0a2d1f4130f65
BenJacobs24 <benjaminjacobs341@gmail.com>
2024-10-30 21:20:46 -0400

    drop down text, added more text, did the footer.

--- script.js
+++ script.js
@@ -14,3 +14,55 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "auto";
     document.getElementById("bar").style.marginLeft = "0px";
   }
+
+  document.querySelector('.toggle-header').addEventListener('click', function() {
+    const info = document.querySelector('.background-info');
+    const arrow = document.querySelector('.toggle-arrow');
+
+    if (info.style.display === 'none' || info.style.display === '') {
+      info.style.display = 'block';
+      arrow.innerHTML = '&#9650;';
+    } else {
+      info.style.display = 'none';
+      arrow.innerHTML = '&#9660;';
+    }
+  });
+
+  document.querySelector('.toggle-header-two').addEventListener('click', function() {
+    const info = document.querySelector('.contributions-info');
+    const arrow = document.querySelector('.toggle-arrow-two');
+
+    if (info.style.display === 'none' || info.style.display === '') {
+      info.style.display = 'block';
+      arrow.innerHTML = '&#9650;';
+    } else {
+      info.style.display = 'none';
+      arrow.innerHTML = '&#9660;';
+    }
+  });
+
+  document.querySelector('.toggle-header-three').addEventListener('click', function() {
+    const info = document.querySelector('.future-info');
+    const arrow = document.querySelector('.toggle-arrow-three');
+
+    if (info.style.display === 'none' || info.style.display === '') {
+      info.style.display = 'block';
+      arrow.innerHTML = '&#9650;';
+    } else {
+      info.style.display = 'none';
+      arrow.innerHTML = '&#9660;';
+    }
+  });
+
+  document.querySelector('.toggle-header-four').addEventListener('click', function() {
+    const info = document.querySelector('.now-info');
+    const arrow = document.querySelector('.toggle-arrow-four');
+
+    if (info.style.display === 'none' || info.style.display === '') {
+      info.style.display = 'block';
+      arrow.innerHTML = '&#9650;';
+    } else {
+      info.style.display = 'none';
+      arrow.innerHTML = '&#9660;';
+    }
+  });
